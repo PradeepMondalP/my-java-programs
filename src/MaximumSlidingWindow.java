59360668536f423fd83671d4718c8a83d8b46d53
Pradeep Mondal <Pradeepmondal1995@gmail.com>
2020-11-11 20:58:55 +0530

    General programs

--- src/MaximumSlidingWindow.java
+++ src/MaximumSlidingWindow.java
@@ -7,11 +7,12 @@ public class MaximumSlidingWindow {
         int arr[] = {1,2,3,1,4,5,2,3,6};
         int k=3 , n=arr.length;
         
-        List<Integer>res = findKMaxElement(arr, k ,n );
+        List<Integer>res = findKMaxElementUsingDeque(arr, k ,n );
         
         for(int x : res) System.out.print(x+" ");
     }
 
+    // method 1  using AVL Tree
     static ArrayList<Integer> findKMaxElement(int[] arr, int k , int n) {
         
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
@@ -38,5 +39,38 @@ public class MaximumSlidingWindow {
         
         // this code is Contributed by Pradeep Mondal P
         
+    }
+    
+    // method 2 using Double ended queue
+    static ArrayList<Integer> findKMaxElementUsingDeque(int[] arr, int k , int n)
+    {
+        ArrayList<Integer>res = new ArrayList<>();
+        Deque<Integer>que = new LinkedList<>();
+        que.addLast(0);
+        int i=1;
+        System.out.println("peekLast "+ arr[que.peekLast()]);
+        
+        for( ; i<k ; i++){
+            while(que.size()>0 && arr[i]>=arr[que.peekLast()]) 
+                que.removeLast();
+            
+            que.addLast(i);
+        }
+        
+        res.add(arr[que.peekFirst()]);
+        
+        for( ; i<n ; i++)
+        {
+            int minReqInd = i-k;
+            while(que.size()>0 && que.peekFirst()<= minReqInd)  que.removeFirst();
+            
+            while(que.size()>0 && arr[que.peekLast()]<=arr[i])  que.removeLast();
+            
+            que.add(i);
+            
+            res.add(arr[que.peekFirst()]);
+        }
+        
+        return res;
     }
 }
