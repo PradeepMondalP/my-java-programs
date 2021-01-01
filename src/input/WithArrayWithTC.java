3bccb13b64341a929bbdcd48675a5a593255e83e
Pradeep Mondal <Pradeepmondal1995@gmail.com>
2021-01-01 10:44:35 +0530

    General programs

--- src/input/WithArrayWithTC.java
+++ src/input/WithArrayWithTC.java
@@ -7,16 +7,19 @@ public class WithArrayWithTC {
     
     public static void main(String[] args)throws java.lang.Exception {
         
-        Scanner sc = new Scanner (System.in);
-        int t = sc.nextInt();
-        while(t-->0)
-        {
-            int n = sc.nextInt();
-            int arr[] = new int[n];
-        
-            for(int i=0 ;i<n ; i++) arr[i] =sc.nextInt();
-            
-            
-        }
+        try {
+            Scanner sc = new Scanner (System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+            {
+                int n = sc.nextInt();
+                int arr[] = new int[n];
+
+                for(int i=0 ;i<n ; i++) arr[i] =sc.nextInt();
+
+
+            }
+        } catch ( Exception e ) { 
+          }
     }
 }
