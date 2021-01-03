d98428918a9a5ca03af2cdbce8b31b187a25c227
Pradeep Mondal <Pradeepmondal1995@gmail.com>
2021-01-03 06:08:38 +0530

    General programs

--- src/input/WithArrayWithTC.java
+++ src/input/WithArrayWithTC.java
@@ -19,7 +19,15 @@ public class WithArrayWithTC {
 
 
             }
-        } catch ( Exception e ) { 
-          }
+        } catch ( Exception e ) { }
+    }
+    
+    // gcd
+    static int gcd(int a , int b)
+    {
+        if(a==0) return b;
+        if(b==0) return a;
+        if(a>b) return gcd(a%b, b);
+        return gcd(a, b%a);
     }
 }
